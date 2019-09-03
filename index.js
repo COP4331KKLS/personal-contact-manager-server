a38740cc0f2e3a17bc640eafd3433bcf3d94f145
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2019-09-03 17:12:05 -0400

    add register user route

--- index.js
+++ index.js
@@ -34,17 +34,47 @@ app.enable("trust proxy");
 
 // routes
 app.get('/status', (request,response) => {
-  response.status(200).json(`Server Live: ${Date.now()}`);
+  response.status(200).json(`Server Live${request.url}: ${Date.now()}`);
 });
 
 app.post('/register', (request,response) => {
-  user.registerUser('user', '123');
-  response.status(200).json('NOT IMPLEMENTED');
+  let username = request.headers.username;
+  let password = request.headers.password;
+
+  let result = {
+    'error': '',
+    'message': ''
+  }
+
+  if(username == undefined || username == null || password == undefined || password == null ) {
+    result.error = 'Invalid Headers'
+    response.status(500).json(result);
+    return;
+  }
+
+  if(!isWordAppropriate(username)) {
+    result.error = 'Username contains inappropriate words'
+    response.status(500).json(result);
+    return;
+  }
+
+  user.registerUser(usersCollection, username, password, (isSuccessful, result) => {
+    console.log(isSuccessful)
+    console.log(result)
+  });
+
+  result.message = 'User has been registered'
+  response.status(200).json(result);
 });
 
 app.post('/login', (request,response) => {
   response.status(200).json('NOT IMPLEMENTED');
 });
+
+// helper functions
+const isWordAppropriate = (input) => {
+  return filter.clean(input) == input
+}
 
 // start application
 app.listen(listeningPort, () => {
--- modules/user.js
+++ modules/user.js
@@ -1,7 +1,35 @@
 
+const { sha256 } = require('crypto-hash');
+
+let collection = null;
+
 // export main functions used in route
-exports.registerUser = (username, password) => {
-  console.log(username, password);
+exports.registerUser = (userCollection, username, password, callback) => {
+  collection = userCollection;
+
+  sha256(password).then(
+    result => addUserAndPassword(username, result, callback),
+    error => callback(false, `Internal Server Error ${error}`)
+  );
+}
+
+const addUserAndPassword = (username, password, callback) => {
+  
+  const newUser = {
+    'username': username,
+    'password': password
+  }
+
+  collection.findOne({'username': username}, 'username').then( result => {
+    if(result != null) {
+      callback(false, 'Username is taken')
+    } else {
+      collection.insert(newUser).then( 
+        insertedUser => callback(true, insertedUser),
+        error => callback(false, `Internal Server Error ${error}`)
+      )
+    }
+  });
 }
 
 exports.authenticateUser = (username, password) => {
