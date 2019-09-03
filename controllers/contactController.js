852a6080118166a5f1f92084b76b872889d4fff1
Kevin Rabinowitz <rabinowitzkevin@gmail.com>
2019-09-03 17:24:00 -0400

    create contact route handlers

---
+++ controllers/contactController.js
@@ -0,0 +1,11 @@
+exports.contacts_list = function(req, res) {
+	res.send('NOT IMPLEMENTED: Contact list');
+};
+
+exports.contact_create = function(req, res) {
+	res.send('NOT IMPLEMENTED: Contact create');
+};
+
+exports.contact_delete = function(req, res) {
+	res.send('NOT IMPLEEMENTED: Contact delete');
+};
