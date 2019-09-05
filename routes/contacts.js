307b0ddeb1bf2f74f8f6d98744082d1050500eff
Kevin Rabinowitz <rabinowitzkevin@gmail.com>
2019-09-05 18:22:32 -0400

    add middleware to authenticate request header

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const contractsController = require('../controllers/contactController');
 
 // GET request for list of all Contact
-router.get('/getContactsList', contactsController.contacts_list);
+router.get('/getContactsList', contactsController.authenticate_user, contactsController.contacts_list);
 
 // POST request for creating Contact
-router.post('/addContact', contactsController.contact_create);
+router.post('/addContact', contactsController.authenticate_user, contactsController.contact_create);
 
 // DELETE request for deleting Contact
-router.delete('/deleteContact/:id', contactsController.contact_delete);
+router.delete('/deleteContact/:id', contactsController.authenticate_user, contactsController.contact_delete);
 
 module.exports = router;
