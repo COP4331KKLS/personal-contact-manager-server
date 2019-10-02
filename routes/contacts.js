9308a3bb83d93e31609c8bd173372ecaba6eabc6
Kevin <rabinowitzkevin@gmail.com>
2019-10-02 03:28:46 -0400

    init commit

---
+++ routes/contacts.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contactController');
+
+// GET request for list of all Contacts
+router.get('/getContactsList', contactsController.authenticate_user, contactsController.contacts_list);
+
+// GET request for searching for Contact
+router.get('/searchContact', contactsController.authenticate_user, contactsController.contact_search);
+
+// POST request for creating Contact
+router.post('/addContact', contactsController.authenticate_user, contactsController.contact_create);
+
+// DELETE request for deleting Contact
+router.delete('/deleteContact', contactsController.authenticate_user, contactsController.contact_delete);
+
+module.exports = router;
